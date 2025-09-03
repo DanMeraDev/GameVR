fa288ed914b73aacb4ce7bf39e037c76d19a6298
Juan <juan.benavides@epn.edu.ec>
2025-09-03 17:14:59 -0500

    Fix escena no se abre

--- Escenario Miedo (1)/Assets/Scripts/FlashLight.cs
+++ Escenario Miedo (1)/Assets/Scripts/FlashLight.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Oculus.Interaction;
+using Unity.VisualScripting;
 public class FlashLight : MonoBehaviour
 {
     [Header("Linterna")]
@@ -7,13 +8,20 @@ public class FlashLight : MonoBehaviour
     public float batteryLife = 200f;
     public float batteryDrainRate = 2.2f;
     public float maxBattery = 300f;
+    public AudioClip onOffSound;
+    public GameObject meshConeLigth;
 
+    private AudioSource onOffAudioSource;
     private bool luzEncendida = false;
     private Grabbable grabbable;
 
     void Awake()
     {
         grabbable = GetComponent<Grabbable>();
+        onOffAudioSource = this.transform.AddComponent<AudioSource>();
+        onOffAudioSource.clip = onOffSound;
+        onOffAudioSource.loop = false;
+        meshConeLigth.SetActive(false);
     }
 
     void Start()
@@ -21,8 +29,15 @@ public class FlashLight : MonoBehaviour
         if (linternaLuz != null)
             linternaLuz.enabled = false;
     }
-
+    //Update de la linterna
     void Update()
+    {
+        logicaLinterna();
+    }
+    
+
+    //Secion de funciones
+    private void logicaLinterna()
     {
         // 👉 Detecta si está agarrado con Oculus
         if (grabbable.SelectingPointsCount > 0) // está agarrado
@@ -47,19 +62,22 @@ public class FlashLight : MonoBehaviour
             }
         }
     }
-
     private void EncenderLinterna()
     {
         if (batteryLife > 0)
         {
+            onOffAudioSource.Play();
             luzEncendida = true;
             linternaLuz.enabled = true;
+            meshConeLigth.SetActive(true);
         }
     }
 
     private void ApagarLinterna()
     {
+        onOffAudioSource.Play();
         luzEncendida = false;
+        meshConeLigth.SetActive(false);
         if (linternaLuz != null)
             linternaLuz.enabled = false;
     }
@@ -67,6 +85,6 @@ public class FlashLight : MonoBehaviour
     public void RechargeBattery(float amount = 50f)
     {
         batteryLife = Mathf.Clamp(batteryLife + amount, 0f, maxBattery);
-        Debug.Log("🔋 Batería recargada. Nivel actual: " + batteryLife);
+        Debug.Log("Batería recargada. Nivel actual: " + batteryLife);
     }
 }

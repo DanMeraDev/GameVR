c82e66360cf93bbc72ea3418e37ddfa5353f66f9
Juan <juan.benavides@epn.edu.ec>
2025-09-04 17:21:43 -0500

    Merge 04/09/25

--- Escenario Miedo (1)/Assets/Scripts/FlashLight.cs
+++ Escenario Miedo (1)/Assets/Scripts/FlashLight.cs
@@ -1,31 +1,39 @@
-using UnityEngine;
 using Oculus.Interaction;
+using Oculus.Interaction.Editor;
+using System.Drawing;
 using Unity.VisualScripting;
+using UnityEngine;
+
 public class FlashLight : MonoBehaviour
 {
     [Header("Linterna")]
     public Light linternaLuz;
+    private Renderer render;
     public float batteryLife = 200f;
     public float batteryDrainRate = 2.2f;
     public float maxBattery = 300f;
     public AudioClip onOffSound;
     public GameObject meshConeLigth;
-
+    public UnityEngine.Color emisionColor = UnityEngine.Color.white;
     private AudioSource onOffAudioSource;
     private bool luzEncendida = false;
     private Grabbable grabbable;
 
     void Awake()
     {
+        render = GetComponent<Renderer>();
         grabbable = GetComponent<Grabbable>();
         onOffAudioSource = this.transform.AddComponent<AudioSource>();
         onOffAudioSource.clip = onOffSound;
         onOffAudioSource.loop = false;
         meshConeLigth.SetActive(false);
+        render.materials[0].SetColor("_EmissionColor", emisionColor * 1f);
+
     }
 
     void Start()
     {
+
         if (linternaLuz != null)
             linternaLuz.enabled = false;
     }
@@ -42,11 +50,13 @@ public class FlashLight : MonoBehaviour
         // 👉 Detecta si está agarrado con Oculus
         if (grabbable.SelectingPointsCount > 0) // está agarrado
         {
+            render.materials[0].DisableKeyword("_EMISSION");
             if (!luzEncendida)
                 EncenderLinterna();
         }
         else
         {
+            render.materials[0].EnableKeyword("_EMISSION");
             if (luzEncendida)
                 ApagarLinterna();
         }

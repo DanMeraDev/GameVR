178ef3d333b72a458a34e894eca12434bac273d9
Juan <juan.benavides@epn.edu.ec>
2025-09-25 20:35:01 -0500

    Habilitacion de checkpoint lvl3 escenario Ira

---
+++ Escenario Miedo (1)/Assets/Scripts/WrathGameScripts/UnlockCheckPoint.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class UnlockCheckPoint : MonoBehaviour
+{
+    private WrathGameManager gameManager;
+    [Header("Posición de respawn de pelota en checkpoint")]
+    public Vector3 checkpointPosition= new Vector3(152.41f, 10.528f, 82.49f);
+    [Header("Objeto que desbloquear (Opcional)")]
+    public GameObject obstacle;
+    public ParticleSystem checkpointEffect;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        gameManager = FindFirstObjectByType<WrathGameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name =="Ball" || other.name == "BallTest") 
+        {
+             gameManager.respawnPointBall = checkpointPosition;
+             gameManager.PlayCheckpointSound();
+        }
+        if (other.CompareTag("Player") && obstacle!=null)
+        {
+            checkpointEffect.Play();
+            obstacle.SetActive(false);
+
+        }
+
+    }
+
+}

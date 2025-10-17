ba7f8cc27ed4518ce39689fe26f094d814fec38b
Juan <juan.benavides@epn.edu.ec>
2025-10-17 17:19:52 -0500

    Vibración en objetos

--- Escenario Miedo (1)/Assets/Scripts/GeneralScripts/GrapWithHapticSource.cs
+++ Escenario Miedo (1)/Assets/Scripts/GeneralScripts/GrapWithHapticSource.cs
@@ -1,32 +1,94 @@
 using Oculus.Haptics;
 using Oculus.Interaction;
+using Oculus.Interaction.Input;
+using Oculus.Interaction.HandGrab;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
-[RequireComponent(typeof(Grabbable))]
-[RequireComponent(typeof(HapticSource))]
 public class GrapWithHapticSource : MonoBehaviour
 {
-    private Grabbable grabbable;
-    private HapticSource hapticSource;
+    private GrabInteractable grabInteractable;
+    [Header("Configuración de Vibración")]
+    [Tooltip("Intensidad de la vibración (0 a 1).")]
+    [Range(0f, 1f)]
+    public float vibrationAmplitude = 0.5f;
+
+    [Tooltip("Frecuencia de la vibración (0 a 1).")]
+    [Range(0f, 1f)]
+    public float vibrationFrequency = 1f;
+
+    [Tooltip("Duración de la vibración en segundos.")]
+    public float vibrationDuration = 0.1f;
+    [Header("Configuración de Vibración")]
+    public AudioClip vibrationClip;
+    private AudioSource vibrationSource;
 
     private void Awake()
     {
-        grabbable = GetComponent<Grabbable>();
-        hapticSource = GetComponent<HapticSource>();
+
+        grabInteractable= GetComponent<GrabInteractable>();
+        vibrationSource = this.transform.AddComponent<AudioSource>();
+
     }
 
     private void OnEnable()
     {
-        grabbable.WhenPointerEventRaised += OnGrabbed;
+        grabInteractable.WhenSelectingInteractorAdded.Action += onInteraction;
+      //  grabbable.WhenSelectingInteractorAdded += OnGrabbed;
 
     }
 
     private void OnDisable()
     {
-        grabbable.WhenPointerEventRaised -= OnGrabbed;
+        // grabbable.WhenPointerEventRaised -= OnGrabbed;
+        grabInteractable.WhenSelectingInteractorAdded.Action -= onInteraction;
 
     }
+    private void onInteraction(GrabInteractor interactor)
+    {
+        Debug.Log("Log intro");
+        var interactorMono = interactor as MonoBehaviour;
+        if (interactorMono == null) return;
+        Debug.Log("Log intro2");
+        // 1. Imprime el nombre del GameObject para saber cuál es.
+        Debug.Log($"El nombre del interactor es: {interactorMono.gameObject.name}");
+
+        // 2. Imprime el objeto mismo para poder hacerle clic en la consola y verlo en la jerarquía.
+        Debug.Log("Haz clic aquí para seleccionar el objeto interactor:", interactorMono.gameObject);
+        HandGrabInteractor handInteractor = interactorMono.GetComponent<HandGrabInteractor>();
+        Debug.Log("Log handInteractor");
+        Debug.Log(handInteractor);
+        if (handInteractor != null)
+        {
+            Debug.Log("INtro Al vibrador");
+            OVRInput.Controller controllerToVibrate = OVRInput.Controller.None;
+
+            if (handInteractor.Hand.Handedness == Handedness.Left)
+            {
+                
+                controllerToVibrate = OVRInput.Controller.LTouch;
+            }
+            else if (handInteractor.Hand.Handedness == Handedness.Right)
+            {
+                controllerToVibrate = OVRInput.Controller.RTouch;
+            }
+
+            if (controllerToVibrate != OVRInput.Controller.None)
+            {
+                Debug.Log("Vibrador");
+                vibrationSource.PlayOneShot(vibrationClip, 1f);
+                // Inicia la vibración en el control correcto
+                OVRInput.SetControllerVibration(vibrationFrequency, vibrationAmplitude, controllerToVibrate);
+
+                // Programa la detención de la vibración después de 'vibrationDuration' segundos
+                Invoke(nameof(StopHaptics), vibrationDuration);
+            }
+        }
+    }
+
+
+
 
     private void OnGrabbed(PointerEvent evt)
     {
--- Escenario Miedo (1)/Assets/Scripts/LoadScebeScripts/LoadSceneScript.cs
+++ Escenario Miedo (1)/Assets/Scripts/LoadScebeScripts/LoadSceneScript.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.ProBuilder;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
